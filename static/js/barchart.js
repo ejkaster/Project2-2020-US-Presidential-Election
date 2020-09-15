4a211624c1dae3f03b4cb348b93b024268e3a581
martinezmaria <helen.martinez@hotmail.com>
2020-09-15 10:21:56 -0500

    pushing final barchart.js

--- static/js/barchart.js
+++ static/js/barchart.js
@@ -355,15 +355,34 @@ chartGroup2.append("text")
 
 });
 
-//####################### CHART 1 - ELECTORAL VOTES (TRUMP) #######################################
+//############################ CHART 3 - ELECTORAL MULTI-BAR CHART  #####################################
+// Define SVG area dimensions
+const svgWidth3 = 860;
+const svgHeight3 = 700;
+
+// Define the chart's margins as an object
+const chartMargin3 = {
+  top: 50,
+  right: 75,
+  bottom: 200,
+  left: 75
+};
+
+// Define dimensions of the chart area
+const chartWidth3 = svgWidth3 - chartMargin3.left - chartMargin3.right;
+const chartHeight3 = svgHeight3 - chartMargin3.top - chartMargin3.bottom;
+
+
+// Select div-id=bar-popular, append SVG area to it, and set the dimensions
 const svg3 = d3.select("#bar-electoral")
   .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
+  .attr("height", svgHeight3)
+  .attr("width", svgWidth3);
 
-// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+
+// // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
 const chartGroup3 = svg3.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+  .attr("transform", `translate(${chartMargin3.left}, ${chartMargin3.top})`);
  
 // Load data from project2_ MongoDB
 const dataUrl3 = '/api/v1.0/national';
@@ -376,246 +395,236 @@ const summaryData3 = electionData3.map(function(d,i) {
 
   return {
     date: d.modeldate,
-    ev_inc: +Math.round(d.ev_inc),
-    ev_chal: +Math.round(d.ev_chal)
+    Trump: +Math.round(d.ev_inc),
+    Biden: +Math.round(d.ev_chal)
   };
 });
 console.log(summaryData3[0]);
 
+
 // Filter for last month of data
-const lastWeek = summaryData3.filter(function(d) { return d.date > '8/1/20'; });
-console.log(lastWeek);
+const lastMonth = summaryData3.filter(function(d) { return d.date > '8/1/20'; });
+console.log(lastMonth);
+
+// Draw chart - sample 1
+
+// List of subgroups - headers
+var subgroups = d3.keys(lastMonth[0]).filter(function(d) {
+  return d != "date";
+});
+console.log(subgroups);
 
 
-// // Create scales
-// x scale
-let xScale3 = d3.scaleBand()
-              .domain(lastWeek.map(row=>row.date))
-              .range([0, chartWidth])
-              .paddingInner(.5);
+// // // List of groups =  value of the first column called date -> I show them on the X axis
+var groups = d3.map(lastMonth, function(d){return(d.date)}).keys()
+console.log(groups);
 
-// y scale
-let yScale3 = d3.scaleLinear()
-              .domain([0, 325])
-              .range([chartHeight, 0]);
+// // // Add X axis
+// The scale spacing the groups:
+var x0 = d3.scaleBand()
+    .rangeRound([0, chartWidth3])
+    .paddingInner(0.085);
 
-// // create and customize axes
-let xAxis3 = d3.axisBottom(xScale3);
-let yAxis3 = d3.axisLeft(yScale3).ticks(10);
+// The scale for spacing each group's bar:
+var x1 = d3.scaleBand()
+    .padding(0.08);
+
+var y = d3.scaleLinear()
+    .rangeRound([chartHeight3, 0]);
+
+// color palette = one color per subgroup
+var z = d3.scaleOrdinal()
+    .range(["red", "lightblue"]);
+
+    x0.domain(lastMonth.map(function(d) { return d.date; }));
+    x1.domain(subgroups).rangeRound([0, x0.bandwidth()]);
+    y.domain([0, 400]);
+
+// //  // Show the bars
+chartGroup3.append("g")
+.selectAll("g")
+.data(lastMonth)
+.enter().append("g")
+.attr("transform", function(d) { return "translate(" + x0(d.date) + ",0)"; })
+.style('fill', 'white')
+.selectAll("rect")
+.data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+.enter().append("rect")
+  .attr("x", function(d) { return x1(d.key); })
+  .attr("y", function(d) { return y(d.value); })
+  .attr("width", x1.bandwidth())
+  .attr("height", function(d) { return chartHeight3 - y(d.value); })
+  .attr("fill", function(d) { return z(d.key); });
+  
+
+  chartGroup3.append("g")
+      .attr("transform", "translate(0," + chartHeight3 + ")")
+      .attr('fill', 'white')
+      .call(d3.axisBottom(x0))
+      .selectAll('text')
+      .attr('transform', 'translate(-10,10)rotate(-90)')
+      .style('text-anchor', 'end')
+      .style('font-size', 14)
+      .style('fill', 'white');
+
+  chartGroup3.append("g")
+      .attr("class", "y axis")
+      .attr('fill', 'white')
+      .call(d3.axisLeft(y).ticks(20))
+      .selectAll('text')
+      .style('text-anchor', 'end')
+      .style('font-size', 12)
+      .style('fill', 'white')
+    .append("text")
+      .attr("x", 2)
+      .attr("y", y(y.ticks().pop()) + 0.5)
+      .attr("dy", "0.32em")
+      .style("fill", "white")
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "start")
+      .text("Electoral Vote Count");
 
 
-// // render axes with .call
-chartGroup3.append('g')
-            .call(xAxis3)
-            .attr('transform', `translate(0, ${chartHeight})`)
-                .selectAll('text')
-                .attr('transform', 'translate(-10,10)rotate(-90)')
-                .style('text-anchor', 'end')
-                .style('font-size', 12)
-                .style('fill', 'white');
-                
 
-chartGroup3.append('g').call(yAxis3)
-            .selectAll('text')
-            .style('text-anchor', 'end')
-            .style('font-size', 12)
-            .style('fill', 'white');
-
-// // create svg rectangle per piece of data
-chartGroup3.selectAll('rect')
-    .data(lastWeek)
-    .enter()
-    .append('rect')
-    .attr('fill', 'red')
-    .attr('opacity', '.75')
-    .attr('x', d => xScale3(d.date))
-    .attr('y', d => yScale3(d.ev_inc))
-    .attr('width', xScale3.bandwidth())
-    .attr('height', d => chartHeight - yScale3(d.ev_inc))
-    .on('mouseover', function (){
-        tooltip.style('display', null);
-    })
-    .on('mouseout', function() {
-        tooltip.style('display', 'none');
-    })
-    .on('mousemove', function (d) {
-        var xPos = d3.mouse(this)[0] - 75;
-        var yPos = d3.mouse(this)[1] - 50; 
-        tooltip.attr('transform', 'translate(' + xPos + ',' + yPos + ')');
-        tooltip.select('text').text(d.date + ':' + d.ev_inc + '' + 'votes');
-
-     });      
-      
-      
-// // Y axis label:
+      // Y axis label:
 chartGroup3.append("text")
 .attr("text-anchor", "end")
 .attr("transform", "rotate(-90)")
-.attr("y", -chartMargin.left+10)
-.attr("x", -chartMargin.top-100)
-.text("Vote Count")
+.attr("y", -chartMargin3.left+20)
+.attr("x", -chartMargin3.top-50)
+.text("Electoral Vote Count")
 .attr('fill', 'white');
 
-// // Add chart title:
+// Add chart label:
 chartGroup3.append("text")
-        .attr("x", (chartWidth / 2))             
-        .attr("y", 0 - (chartMargin.top / 2))
+        .attr("x", (chartWidth3 / 2))             
+        .attr("y", 0 - (chartMargin3.top / 2))
         .attr("text-anchor", "middle")  
-        .style("font-size", "18px") 
-        .style('font-weight', 'bold')  
+        .style("font-size", "16px") 
         .attr('fill', 'white')
-        .text("Forecasted electoral vote count - Trump");
+        .text("Forecasted Electoral Vote Count");
 
+   
+var legend = chartGroup3.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(subgroups.slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 30 + ")"; });
+      legend.append("rect")
+      .attr("x", chartWidth3 - 15)
+      .attr("width", 15)
+      .attr("height", 15)
+      .attr("fill", z)
+      .attr("stroke", z)
+      .attr("stroke-width",2)
+      .on("click",function(d) { update(d) });
+
+  legend.append("text")
+      .attr("x", chartWidth3 - 24)
+      .attr("y", 12)
+      .attr("dy", "0.32em")
+      .attr('fill', 'white')
+      .attr('font-weight', 'bold')
+      .text(function(d) { return d; });
+
+
+// });
+
+
+      var filtered = [];
+
+  ////
+  //// Update and transition on click:
+  ////
+  
+  function update(d) {  
+   
+    //
+    // Update the array to filter the chart by:
+    //
+   
+    // add the clicked key if not included:
+    if (filtered.indexOf(d) == -1) {
+     filtered.push(d); 
+      // if all bars are un-checked, reset:
+      if(filtered.length == subgroups.length) filtered = [];
+    }
+    // otherwise remove it:
+    else {
+      filtered.splice(filtered.indexOf(d), 1);
+    }
     
-
-const tooltip = chartGroup3.append('g')
-        .style('display', 'none')
-        .style('background-color', 'white')
-        .style('border', 'solid')
-        .style('border-width', '2px')
-        .style('border-radius', '5px')
-        .style('padding', '5px');
-
-        tooltip.append('text')
-            .attr('x', 15)
-            .attr('dy', '1.2em')
-            .style('font-size', '13')
-            .style('fill', 'white')
-            .attr('font-weight', 'bold');
-
-
-});
-
-
-// //############################ CHART 2 - ELECTORAL VOTES (BIDEN) #####################################
-const svg4 = d3.select("#bar-electoral")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
-
-// // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
-const chartGroup4 = svg4.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
- 
-// // Load data from project2_ MongoDB
-const dataUrl4 = '/api/v1.0/national';
-
-const electionData4 = d3.json(dataUrl4).then(electionData4 => {
-              console.log(electionData4[0]);
-              console.log(electionData4.length);
-
-const summaryData4 = electionData4.map(function(d,i) {
-
-  return {
-    date: d.modeldate,
-    ev_inc: +Math.round(d.ev_inc),
-    ev_chal: +Math.round(d.ev_chal)
-  };
-});
-console.log(summaryData4[0]);
-
-// // Filter for last month of data
-const lastWeek2 = summaryData4.filter(function(d) { return d.date > '8/1/20'; });
-console.log(lastWeek2);
-
-
-// // Create scales
-// // x scale
-let xScale4 = d3.scaleBand()
-              .domain(lastWeek2.map(row=>row.date))
-              .range([0, chartWidth])
-              .paddingInner(.5);
-
-// // y scale
-let yScale4 = d3.scaleLinear()
-              .domain([0, 325])
-              .range([chartHeight, 0]);
-
-// // create and customize axes
-let xAxis4 = d3.axisBottom(xScale4);
-
-let yAxis4 = d3.axisLeft(yScale4).ticks(10);
-
-
-// // render axes with .call
-chartGroup4.append('g')
-            .call(xAxis4)
-            .attr('transform', `translate(0, ${chartHeight})`)
-                .selectAll('text')
-                .attr('transform', 'translate(-10,10)rotate(-90)')
-                .style('text-anchor', 'end')
-                .style('font-size', 12)
-                .style('fill', 'white');
-                
-
-chartGroup4.append('g').call(yAxis4)
-            .selectAll('text')
-            .style('text-anchor', 'end')
-            .style('font-size', 12)
-            .style('fill', 'white');
-
-// // create svg rectangle per piece of data
-chartGroup4.selectAll('rect')
-    .data(lastWeek2)
-    .enter()
-    .append('rect')
-    .attr('fill', 'lightblue')
-    .attr('opacity', '.75')
-    .attr('x', d => xScale4(d.date))
-    .attr('y', d => yScale4(d.ev_chal))
-    .attr('width', xScale4.bandwidth())
-    .attr('height', d => chartHeight - yScale4(d.ev_chal))
-    .on('mouseover', function (){
-        tooltip1.style('display', null);
+    //
+    // Update the scales for each group(/dates)'s items:
+    //
+    var newKeys = [];
+    subgroups.forEach(function(d) {
+      if (filtered.indexOf(d) == -1 ) {
+        newKeys.push(d);
+      }
     })
-    .on('mouseout', function() {
-        tooltip1.style('display', 'none');
-    })
-    .on('mousemove', function (d) {
-        var xPos1 = d3.mouse(this)[0] - 75;
-        var yPos1 = d3.mouse(this)[1] - 50; 
-        tooltip1.attr('transform', 'translate(' + xPos1 + ',' + yPos1 + ')');
-        tooltip1.select('text').text(d.date + ':' + d.ev_chal + ''+ 'votes');
-
-
-      });      
-      
-      
-// // Y axis label:
-chartGroup4.append("text")
-.attr("text-anchor", "end")
-.attr("transform", "rotate(-90)")
-.attr("y", -chartMargin.left+10)
-.attr("x", -chartMargin.top-100)
-.text("Vote Count")
-.attr('fill', 'white');
-
-// // Add chart title:
-chartGroup4.append("text")
-        .attr("x", (chartWidth / 2))             
-        .attr("y", 0 - (chartMargin.top / 2))
-        .attr("text-anchor", "middle")  
-        .style("font-size", "18px") 
-        .style('font-weight', 'bold')  
-        .attr('fill', 'white')
-        .text("Forecasted electoral vote count - Biden");
-
+    x1.domain(newKeys).rangeRound([0, x0.bandwidth()]);
+    y.domain([0, d3.max(lastMonth, function(d) { return d3.max(subgroups, function(key) { if (filtered.indexOf(key) == -1) return d[key]; }); })]).nice();
     
+    // update the y axis:
+            chartGroup3.select(".y")
+            .transition()
+            .call(d3.axisLeft(y).ticks(null, "s"))
+            .duration(500);
+    
+// Filter out the bands that need to be hidden:
+    //
+    var bars = chartGroup3.selectAll(".bar").selectAll("rect")
+      .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+    
+   bars.filter(function(d) {
+         return filtered.indexOf(d.key) > -1;
+      })
+      .transition()
+      .attr("x", function(d) {
+        return (+d3.select(this).attr("x")) + (+d3.select(this).attr("width"))/2;  
+      })
+      .attr("height",0)
+      .attr("width",0)     
+      .attr("y", function(d) { return chartHeight; })
+      .duration(500);
+            
+    //
+    // Adjust the remaining bars:
+    //
+    bars.filter(function(d) {
+        return filtered.indexOf(d.key) == -1;
+      })
+      .transition()
+      .attr("x", function(d) { return x1(d.key); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return chartHeight - y(d.value); })
+      .attr("width", x1.bandwidth())
+      .attr("fill", function(d) { return z(d.key); })
+      .duration(500);
 
-const tooltip1 = chartGroup4.append('g')
-        .style('display', 'none')
-        .style('background-color', 'white')
-        .style('border', 'solid')
-        .style('border-width', '2px')
-        .style('border-radius', '5px')
-        .style('padding', '5px');
+ // update legend:
+ legend.selectAll("rect")
+ .transition()
+ .attr("fill",function(d) {
+   if (filtered.length) {
+     if (filtered.indexOf(d) == -1) {
+       return z(d); 
+     }
+      else {
+       return "white"; 
+     }
+   }
+   else {
+    return z(d); 
+   }
+ })
+ .duration(100);
 
-        tooltip1.append('text')
-            .attr('x', 15)
-            .attr('dy', '1.2em')
-            .style('font-size', '13')
-            .style('fill', 'white')
-            .attr('font-weight', 'bold');
 
+}
 
 });
